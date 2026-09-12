c0bb7a6747b1716d0dc8795bb9aaa7bbe0084bbc
Gin-356A <Gin_356A@proton.me>
2026-09-12 13:42:15 +0530

    Card functionality in process

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 import { menuArray  } from "./data.js"
 
-
+let total = 0
 const items = document.querySelector(".items")
+const billList = document.querySelector(".bill-list")
+const order =[]
 
-console.log(items)
+document.addEventListener('click', function(e){
+    if(e.target.dataset.id){
+        //addItem(e.target.dataset.id , Number(e.target.dataset.price) )
+
+        addOrderToArr(e.target.dataset.id , e.target.dataset.price 
+            , e.target.dataset.name
+        )
+    }
+})
+// function addItem(id , price){
+//     total += price
+//     console.log(total)
+// }
+function addOrderToArr(id , price , name){
+    order.push(
+        {
+            name: name ,
+            price: price ,
+            id : id,
+            count: 0
+        }
+    )
+    console.log(order)
+}
 
 function render(){
     let text = ""
 
     menuArray.forEach((item)=> {
-        const {name , price , emoji} = item
+        const {name , price , emoji , id} = item
         const ingredients = item.ingredients.join(" ")
         text += `
         <li class="item">
@@ -19,7 +44,13 @@ function render(){
                 <p>${ingredients}</p>
                 <p><span>$${price}</span></p>
             </div>
-            <button class="add-btn">+</button>
+            <button class="add-btn" 
+                data-id=${id} 
+                data-price=${price} 
+                data-name=${name}
+                >
+                +
+                </button>
         </li>
         `
     })
