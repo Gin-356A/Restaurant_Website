e10fc4d09e41440373bd75c79acdfab7086bd36e
Gin-356A <Gin_356A@proton.me>
2026-09-17 13:58:21 +0530

    Changed the drink name

--- data.js
+++ data.js
@@ -14,7 +14,7 @@ export const menuArray = [
         id: 1
     },
         {
-        name: "Beer",
+        name: "Coke",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
